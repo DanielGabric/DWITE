317ee5c68236e0d1909ae98d79a8d91bb70c5489
Dan <danielgabric@live.com>
2015-04-22 14:18:20 -0400

    .

--- WeakPasswords.c
+++ WeakPasswords.c
@@ -8,11 +8,8 @@ int main()
         char a1,a2,a3,a4;
         int m,k;
         for(a1='A';a1<='Z';++a1)
-        {
             for(a2='A';a2<='Z';++a2)
-            {
                 for(a3='A';a3<='Z';++a3)
-                {
                     for(a4='A';a4<='Z';++a4)
                     {
                         k = a1*1000000+a2*10000+a3*100+a4;
@@ -23,9 +20,6 @@ int main()
                             goto out;
                         }
                     }
-                }
-            }
-        }
         out:
         a1=a2=a3=a4='A';
     }
